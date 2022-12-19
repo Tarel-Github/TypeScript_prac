94e0e86f184827ff19ce6b53bd2248bc29ef3e13
tarel <sms_stmax@naver.com>
2022-12-19 19:58:02 +0900

    [연습] 블록체인

--- src/index.ts
+++ src/index.ts
@@ -30,5 +30,39 @@ class Block implements BlockShape {
   }
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height} ${data}}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlock() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+blockchain.addBlock("Fourth one");
+
+blockchain.getBlock().push(new Block("x", 11111, "법규"));
+
+console.log(blockchain.getBlock());
