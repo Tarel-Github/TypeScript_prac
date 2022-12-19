8fcbbce36cc14ef85849cb73a16e4c1f5ef1b6b3
tarel <sms_stmax@naver.com>
2022-12-19 16:06:46 +0900

    [연습]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-//myPackage.js 파일과 myPackage.d.ts 파일에서 가져온다
-//myPackage.d.ts파일에서, init은 bool 타입이라고 정의됌
-import { init, exit } from "./myPackage";
+// //myPackage.js 파일과 myPackage.d.ts 파일에서 가져온다
+// //myPackage.d.ts파일에서, init은 bool 타입이라고 정의됌
+// import { init, exit } from "myPackage";
 
-init({
-  url: "true",
-});
+// init({
+//   url: "true",
+// });
 
-exit(1);
+// exit(1);
 
-localStorage.clear();
+// localStorage.clear();
+
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height} ${data}}`;
+  }
+}
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,21 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+
 export function init(config) {
   return true;
 }
+
+/**
+ * 프로그램을 종료한다.
+ * @param {number} code
+ * @returns {number}
+ */
 export function exit(code) {
   return code + 1;
 }
